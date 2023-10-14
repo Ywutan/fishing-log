739dbdde7c5e20947c16efc09d2abce1876e8e6b
yves vang <yvangyeng@gmail.com>
2023-10-14 14:15:22 -0400

    next js environment installed

---
+++ src/app/layout.js
@@ -0,0 +1,11 @@
+function Layout({children}) {
+    return (
+        <html>
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
+
+export default Layout;
---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+function Page() {
+    return (
+        <div>
+            Main Page
+        </div>
+    );
+}
+
+export default Page;
